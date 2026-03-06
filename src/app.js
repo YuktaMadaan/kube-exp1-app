60143d292f80a9a9e0aae674cc9e2e5da8d92354
Yukta <146582577+YuktaMadaan@users.noreply.github.com>
2026-03-06 15:08:15 +0530

    Change response message from 'GitOps great' to 'GitOps changed'

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GitOps great");
+  res.send("GitOps changed");
 });
 
 app.listen(3000, () => {
